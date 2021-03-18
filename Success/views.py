733fb056ed72e25650024fafa4b10240ca5fe276
zecollokaris <collo.kariss@gmail.com>
2021-03-18 22:20:46 +0300

    added login required decorator to restrict site access

--- Success/views.py
+++ Success/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 
-
+from django.contrib.auth.decorators import login_required
 #---------------------------------------------------------------------#
 '''End Of Import'''
 #---------------------------------------------------------------------#
@@ -15,6 +15,7 @@ from django.shortcuts import render, redirect
 #################################################################################################################################################################################
 
 #Home page view function
+@login_required
 def index(request):
 	title = 'Welcome: This is the Home Page'
 	context = {
