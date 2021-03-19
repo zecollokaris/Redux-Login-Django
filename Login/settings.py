823692ebf66a094eea96f7614e3adb531e1a8f0b
zecollokaris <collo.kariss@gmail.com>
2021-03-19 17:28:39 +0300

    deployment attempt

--- Login/settings.py
+++ Login/settings.py
@@ -129,7 +129,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ACCOUNT_ACTIVATION_DAYS = 7 # One-Week Activation Expiry
 REGISTRATION_AUTO_LOGIN = True # Automatically Log the User In.
